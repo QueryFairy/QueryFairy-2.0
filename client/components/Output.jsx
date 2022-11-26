2bea979f9c11ea192925b6af460a250cd63095d8
Ryan <ryanzarou@gmail.com>
2022-11-26 13:30:25 -0500

    set up code mirror windows for visualizer and output components

--- client/components/Output.jsx
+++ client/components/Output.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { dracula } from '@uiw/codemirror-theme-dracula';
+
 const Output = (props) => {
   const { output } = props;
 
   return (
-  <div className='Output'>Output</div>
+  <div className='Output'>
+    <CodeMirror
+    height="175px"
+    value='//Hello'
+    theme={dracula}
+    extensions={[javascript({ jsx: true })]}
+      />
+  </div>
   );
 };
 export default Output;
--- client/components/Visualizer.jsx
+++ client/components/Visualizer.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+// import 'codemirror/keymap/sublime';
+//import { sublime } from '@uiw/codemirror-keymap-sublime';
+import { dracula } from '@uiw/codemirror-theme-dracula';
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+//import { javascript } from 'codemirror/mode/javascript/javascript'
+//const jsx = require('codemirror/mode/javascript/javascript');
+
 
 const Visualizer = (props) => {
   const { visualizer } = props;
 
+  
   return (
-  <div className='Visualizer'>Visualizer</div>
+  <div className='Visualizer'>
+    <CodeMirror
+    height="300px"
+    value='//Hello'
+    theme={dracula}
+    //keymap={sublime}
+    // mode={javascript}
+    extensions={[javascript({ jsx: true })]}
+    // options={{
+    //       }}
+    />
+    </div>
   );
 };
 export default Visualizer;
