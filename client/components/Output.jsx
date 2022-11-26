27595d3b42310c563339a53dbff2f2649516e156
Ryan <ryanzarou@gmail.com>
2022-11-26 15:06:22 -0500

    made codemirror read only and text wrapping + fixed package.json

--- client/components/Output.jsx
+++ client/components/Output.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { dracula } from '@uiw/codemirror-theme-dracula';
+import {EditorView, basicSetup} from "codemirror";
+import {EditorState} from "@codemirror/state";
 
 const Output = (props) => {
   const { output } = props;
@@ -12,7 +14,11 @@ const Output = (props) => {
     height="175px"
     value='//Hello'
     theme={dracula}
-    extensions={[javascript({ jsx: true })]}
+    extensions={
+      [javascript({ jsx: true }), EditorView.lineWrapping, EditorState.readOnly.of(true)]}
+    options={{
+      linewrapping: true
+          }}
       />
   </div>
   );
--- client/components/Visualizer.jsx
+++ client/components/Visualizer.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import { dracula } from '@uiw/codemirror-theme-dracula';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import {EditorView, basicSetup} from "codemirror";
+import {EditorState} from "@codemirror/state"
 //import { javascript } from 'codemirror/mode/javascript/javascript'
 //const jsx = require('codemirror/mode/javascript/javascript');
 
@@ -11,18 +13,24 @@ import { javascript } from '@codemirror/lang-javascript';
 const Visualizer = (props) => {
   const { visualizer } = props;
 
+
+  const text = 'Lorem ipsum dolor sit \n \t amet, consectetur adipiscing elit, \n sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+
   
   return (
   <div className='Visualizer'>
     <CodeMirror
+    value={text}
     height="300px"
-    value='//Hello'
+    width="500px"
     theme={dracula}
     //keymap={sublime}
     // mode={javascript}
-    extensions={[javascript({ jsx: true })]}
-    // options={{
-    //       }}
+    extensions={
+      [javascript({ jsx: true }), EditorView.lineWrapping, EditorState.readOnly.of(true)]}
+    options={{
+      linewrapping: true,
+          }}
     />
     </div>
   );
