50659f7303bf9364ede5d7348f6de785b94f29a4
Francheska Orellana <yudhitza1@gmail.com>
2022-11-30 14:04:47 -0500

    cleaning up code

--- server/db.js
+++ server/db.js
@@ -15,21 +15,3 @@ module.exports = {
     return pool.query(text, params, callback);
   }
 };
-
-CREATE TABLE user(user_pk INT GENERATED ALWAYS AS IDENTITY, first_name VARCHAR(100) NOT NULL, last_name VARCHAR(100) NOT NULL, email VARCHAR(100) NOT NULL UNIQUE, username VARCHAR(100) NOT NULL UNIQUE, password VARCHAR(100) NOT NULL, PRIMARY KEY (user_pk));
-
-
-
-CREATE TABLE query (
-	query_pk INT GENERATED ALWAYS AS IDENTITY,
-	endpoint VARCHAR(255) NOT NULL,
-	user_id INT NOT NULL,
-	PRIMARY KEY(query_pk),
-  CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(user_pk)
-);
-
-
-
-
-ALTER TABLE "queries" ADD CONSTRAINT "queries_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("_id");
-
