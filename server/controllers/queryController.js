e715abb8baadf68ecd99b0ec4398dab3383a7286
Francheska Orellana <yudhitza1@gmail.com>
2022-12-01 00:03:05 -0500

    fetching queries from database successful

--- server/controllers/queryController.js
+++ server/controllers/queryController.js
@@ -6,7 +6,6 @@ const { ModuleFilenameHelpers } = require('webpack');
 const queryController = {};
 
 queryController.saveQuery = async (req,res,next) => {
-  console.log('im in controller');
   const {username, query}= req.body;
   const saveQueryString = `
   INSERT INTO query (endpoint, user_id) 
@@ -24,4 +23,24 @@ queryController.saveQuery = async (req,res,next) => {
   }
 }
 
+queryController.getQueryList = async (req, res, next) => {
+  console.log('im in controller', req.params.id);
+  const user = [req.params.id];
+  const queryString = `SELECT endpoint FROM query WHERE user_id = (SELECT user_pk FROM users WHERE username = $1) `
+  try{
+    const data = await db.query(queryString, user);
+    const result = [];
+    data.rows.forEach(el => {
+      result.push(el.endpoint);
+    })
+    //console.log(result);
+    res.locals.queryList = result;
+  } catch (error) {
+    return next({
+      log: 'error caught in queryController.getQueryList middleware function',
+      message: { err: `${error}, caught when fetching query list`}
+    })
+  }
+  return next()
+}
 module.exports = queryController;
--- server/routes/queryRouter.js
+++ server/routes/queryRouter.js
@@ -7,6 +7,10 @@ router.post('/', queryController.saveQuery, (req,res) => {
   res.sendStatus(200);
 });
 
+router.get('/:id', queryController.getQueryList, (req, res) => {
+  res.status(200).json(res.locals.queryList);
+});
+
 
 
 module.exports = router;
